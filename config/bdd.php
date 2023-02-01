9a273f778aadbcee14fa0d26147bbc0faa553850
Alexoos <95075956+Alexoos@users.noreply.github.com>
2023-02-01 14:50:27 +0100

    Update bdd.php

--- config/bdd.php
+++ config/bdd.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DB_URL', "localhost");
-define('DB_USER', "root");
-define('DB_PWD', "");
+define('DB_USER', "userGsb");
+define('DB_PWD', "LeGroupeDesAlex6");
 define('DB_NAME', "gsb_frais_b3");
 define('DB_DSN', "mysql:host=" . DB_URL . ";dbname=" . DB_NAME . ";charset=UTF8");
