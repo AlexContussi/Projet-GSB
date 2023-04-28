470185fd0e66ea2cc4857140fe65eade60d5594e
Alexoos <95075956+Alexoos@users.noreply.github.com>
2023-04-28 15:33:08 +0200

    Info serveur

--- config/bdd.php
+++ config/bdd.php
@@ -5,3 +5,8 @@ define('DB_USER', "userGsb");
 define('DB_PWD', "LeGroupeDesAlex6");
 define('DB_NAME', "gsb_frais_b3");
 define('DB_DSN', "mysql:host=" . DB_URL . ";dbname=" . DB_NAME . ";charset=UTF8");
+
+
+//connexion serveur
+// user : usergsb
+// mdp : LeGroupeDesAlex6
